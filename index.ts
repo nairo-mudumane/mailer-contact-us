3b5fe851272908ab08bfa3403157658bfdcdf176
nairo-mudumane <nairomudumane@outlook.com>
2022-11-04 15:16:08 +0200

    env variables settup

--- index.ts
+++ index.ts
@@ -1,16 +1,26 @@
 import Fastify from "fastify";
 import cors from "@fastify/cors";
+// import AppRoutes from "./routes";
 
 async function appInit() {
-  const app = Fastify({
-    logger: true,
-    requestTimeout: 600000,
-  });
+  try {
+    const server = Fastify({
+      logger: true,
+      requestTimeout: 600000,
+    });
 
-  await app.register(cors, {
-    origin: true,
-  });
+    await server.register(cors, {
+      origin: true,
+    });
 
-  await app.listen({ port: 3333 });
+    server.get("/mailer/contact-us", (request, reply) => {
+      return reply.code(200).send({ ok: true });
+    });
+
+    await server.listen({ port: 3333 });
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
 }
 appInit();
